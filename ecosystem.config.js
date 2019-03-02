c8e6d7007b148d23981067dca9f2471b9c3f7df1
tomcorey26 <tomcorey26@gmail.com>
2019-03-02 16:36:59 -0500

    server

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tom-server',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ubuntu@ec2-18-218-247-243.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/tom-tutorial.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:tomcorey26/tom-server.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
