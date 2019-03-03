11d0715c984a81dfa64d6fb12f112e80c8325cfc
tomcorey26 <tomcorey26@gmail.com>
2019-03-02 20:49:54 -0500

    lol

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.send('Tim likes suckin lil wieners')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
